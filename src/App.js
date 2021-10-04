637bb21db086a9e7fca9769651a6953bd96635c5
Igwe James <jamesigboman1@yahoo.com>
2021-10-04 13:11:42 +0100

    my file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>my first react program</h1>
+     <h2>just testing on board strings</h2>
+    
+    </div>
+  );
+}
+
+export default App;
